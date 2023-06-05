3dbc47d8c63b73a734127292e7bd151beca3b09e
Tiara <tiaranurulh@gmail.com>
2023-06-05 18:01:13 +0700

    Initial Commit

---
+++ miniTest-Stamps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miniTest-Stamps
+//
+//  Created by Tiara H on 05/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
